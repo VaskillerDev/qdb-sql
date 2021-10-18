74149406f46d3ea1915462f4f1a3ab68ec6a3b25
VaskillerDev <Vaskoooo9241@gmail.com>
2021-10-18 18:25:23 +0300

    minor

--- src/ast/select_statement_single_row.rs
+++ src/ast/select_statement_single_row.rs
@@ -2,4 +2,4 @@ use crate::ast::action::Action;
 use crate::ast::select_list::SelectList;
 use crate::ast::set_quantifier::SetQuantifier;
 
-type SelectStatementSingleRow = (Action, Vec<SetQuantifier>, SelectList);
+type SelectStatementSingleRow = (Action, Option<SetQuantifier>, SelectList);
